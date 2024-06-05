2a188ea8fb046942d50c87c57cdf9f314c9a2fde
Vladislav Golosov <v.golosoff@gmail.com>
2024-06-05 15:56:11 +0300

    Create object ComputerClub

--- ComputerClub/Program.cs
+++ ComputerClub/Program.cs
@@ -4,14 +4,60 @@
     {
         static void Main(string[] args)
         {
-
+            ComputerClub club = new ComputerClub(5);
+            club.Work();
         }
     }
 
     class ComputerClub
     {
+        private int _money = 0;
         private List<Computer> _computers = new List<Computer>();
         private Queue<Client> _clients = new Queue<Client>();
+
+        public ComputerClub(int computersCount)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < computersCount; i++)
+            {
+                _computers.Add(new Computer(random.Next(5,15)));
+            }
+            
+            CreateNewClients(20, random);
+        }
+
+        public void CreateNewClients(int count, Random random)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                _clients.Enqueue(new Client(random.Next(100,300), random));
+            }
+        }
+
+        public void Work()
+        {
+            while (_clients.Count > 0)
+            {
+                Client newClient = _clients.Dequeue();
+                Console.WriteLine($"Баланс компьютерного клуба: {_money} руб. Ждем нового клиента.");
+                Console.WriteLine($"У вас новый клиент, и он хочет купить {newClient.DesiredMinutes} минут");
+                ShowAllComputerState();
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        private void ShowAllComputerState()
+        {
+            Console.WriteLine("\nСписок всех компьютеров:");
+            for (int i = 0; i < _computers.Count; i++)
+            {
+                Console.Write(i + 1 + " - ");
+                _computers[i].ShowState();
+            }
+        }
     }
 
     class Computer
